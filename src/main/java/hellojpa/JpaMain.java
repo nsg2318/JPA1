5d38321eb19f502187b00819665bd6a31f4d73bc
sunba <nsg2318@naver.com>
2021-11-07 17:12:36 +0900

    add : 쓰기지연 관련 예제 커밋

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,25 +8,20 @@ import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
-        //emf의 persistenceUnitName 은 pom.xml의 name "hello"
-        //emf는 애플리케이션 로딩 시점의 딱 1번만 로드되어야 함(DB 당 하나)
-        //em 은 한 작업단위마다 만들어줘야함 마치 컬렉션에 저장해두는 것과 같음
-        //쓰레드간 공유하면 안됨
-        //JPA의 모든 변경은 트랜잭션 안에서 실행
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         //code
         try {
+            JpaMember jpaMember1 = new JpaMember(150L, "a");
+            JpaMember jpaMember2 = new JpaMember(151L, "b");
+            //persist 마다 쿼리를 날리면 최적화 여지가 없다.
+            em.persist(jpaMember1);
+            em.persist(jpaMember2);
 
-            List<JpaMember> result = em.createQuery("select m from JpaMember as m", JpaMember.class)
-                    .getResultList();
-
-            for (JpaMember jpaMember : result) {
-                System.out.println("jpaMember = " + jpaMember.getName());
-            }
-
+            System.out.println("==================");
             tx.commit();
         } catch (Exception e){
             tx.rollback();
--- src/main/java/hellojpa/JpaMember.java
+++ src/main/java/hellojpa/JpaMember.java
@@ -13,6 +13,13 @@ public class JpaMember {
     private Long id;
     private String name;
 
+    public JpaMember(){}
+
+    public JpaMember(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
